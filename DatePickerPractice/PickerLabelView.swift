6a2cbd397cfaffbec2e8122d9223ddff7ce63a5a
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-09-22 06:01:29 +0900

    ファイル分けを行った

--- DatePickerPractice/PickerLabelView.swift
+++ DatePickerPractice/PickerLabelView.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+
 protocol TimeStructure {
     var timeRange: [Int] { get }
     var unit: String { get }
@@ -28,84 +29,4 @@ struct Second: TimeStructure {
     var timeRange: [Int] = [Int](0...59)
     var unit: String = "秒"
     var number: Int = 3
-}
-
-final class PickerLabelView: UIPickerView {
-    
-    private let TimeStructures: [TimeStructure] = [Hour(), Minute(), Second()]
-    private var labels: [UILabel] = []
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        reloadAllComponentLabels()
-    }
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setup()
-    }
-    
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        setup()
-    }
-    
-    private func setup() {
-        self.delegate = self
-        self.dataSource = self
-    }
-    
-}
-
-extension PickerLabelView {
-    
-    private func reloadAllComponentLabels() {
-        let fontSize = CGFloat(15)
-        let labelTop = self.bounds.origin.y + self.bounds.height / 2 - fontSize
-        let labelHeight = self.rowSize(forComponent: 0).height
-        var labelOffset = self.bounds.origin.x
-        // 変更
-        TimeStructures.forEach {
-            let row = $0.number - 1
-            if self.labels.count == row {
-                let label = UILabel()
-                label.text = $0.unit
-                label.font = UIFont.boldSystemFont(ofSize: fontSize)
-                label.sizeToFit()
-                
-                self.addSubview(label)
-                self.labels.append(label)
-            }
-            let labelWidth = self.labels[row].frame.width
-            labelOffset += self.rowSize(forComponent: row).width
-            self.labels[row].frame = CGRect(x: labelOffset - labelWidth,
-                                          y: labelTop,
-                                          width: labelWidth,
-                                          height: labelHeight)
-        }
-    }
-    
-}
-
-extension PickerLabelView: UIPickerViewDataSource {
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        TimeStructures.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView,
-                    numberOfRowsInComponent component: Int) -> Int {
-        TimeStructures[component].timeRange.count
-    }
-    
-}
-
-extension PickerLabelView: UIPickerViewDelegate {
-    
-    func pickerView(_ pickerView: UIPickerView,
-                    titleForRow row: Int,
-                    forComponent component: Int) -> String? {
-        row.description
-    }
-    
 }
--- DatePickerPractice/ViewController.swift
+++ DatePickerPractice/ViewController.swift
@@ -11,9 +11,69 @@ final class ViewController: UIViewController {
 
     @IBOutlet private weak var pickerView: UIPickerView!
     
+    private let TimeStructures: [TimeStructure] = [Hour(), Minute(), Second()]
+    private var labels: [UILabel] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setup()
+        reloadAllComponentLabels()
     }
     
 }
 
+extension ViewController {
+    
+    private func setup() {
+        pickerView.delegate = self
+        pickerView.dataSource = self
+    }
+
+    private func reloadAllComponentLabels() {
+        let fontSize = CGFloat(15)
+        let labelTop = pickerView.bounds.origin.y + pickerView.bounds.height / 2 - fontSize
+        let labelHeight = pickerView.rowSize(forComponent: 0).height
+        var labelOffset = pickerView.bounds.origin.x
+        // 変更
+        TimeStructures.forEach {
+            let row = $0.number - 1
+            if self.labels.count == row {
+                let label = UILabel()
+                label.text = $0.unit
+                label.font = UIFont.boldSystemFont(ofSize: fontSize)
+                label.sizeToFit()
+                pickerView.addSubview(label)
+                self.labels.append(label)
+            }
+            let labelWidth = self.labels[row].frame.width
+            labelOffset += pickerView.rowSize(forComponent: row).width
+            self.labels[row].frame = CGRect(x: labelOffset - labelWidth,
+                                          y: labelTop,
+                                          width: labelWidth,
+                                          height: labelHeight)
+        }
+    }
+
+}
+
+extension ViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        TimeStructures.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView,
+                    numberOfRowsInComponent component: Int) -> Int {
+        TimeStructures[component].timeRange.count
+    }
+    
+    
+}
+
+extension ViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView,
+                    titleForRow row: Int,
+                    forComponent component: Int) -> String? {
+        row.description
+    }
+
+}
